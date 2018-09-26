ef68e7a960caf83404369eb32e7fda43da62e0e3
Martynas Pumputis <m@lambda.lt>
2018-09-26 20:13:42 +0300

    Check for JSON encoding errors

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -32,5 +32,7 @@ func (h *Handler) HandleGetIPTables(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(ipt)
+	if err := json.NewEncoder(w).Encode(ipt); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 }
